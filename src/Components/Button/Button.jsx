44a8f660335be8992487aaf724a6a1a824921f81
Nikola Pavlovic <nikolapavlovic@Nikolas-MacBook-Pro.local>
2019-02-01 18:18:35 +0100

    fix: improved button component and added another button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -3,31 +3,27 @@ import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = ({
-  className, buttonText, buttonIconURL, iconClassName,
-}) => {
-  let buttonIcon;
-  if (buttonIconURL) {
-    buttonIcon = <img alt="" className={iconClassName} src={buttonIconURL} />;
-  }
-  return (
-    <button type="button" className={className}>
-      {buttonText}
-      {buttonIcon}
-    </button>
-  );
-};
+  className, buttonText, buttonIconURL, iconClassName, buttonTextClassName,
+}) => (
+  <button type="button" className={className}>
+    {buttonText && <p className={buttonTextClassName}>{buttonText}</p>}
+    {buttonIconURL && <img alt="" className={iconClassName} src={buttonIconURL} />}
+  </button>
+);
 
 Button.defaultProps = {
-  className: null,
+  buttonTextClassName: null,
   buttonText: null,
+  className: null,
   iconClassName: null,
   buttonIconURL: null,
 };
 
 Button.propTypes = {
+  buttonTextClassName: PropTypes.string,
+  buttonText: PropTypes.string,
   buttonIconURL: PropTypes.string,
   className: PropTypes.string,
-  buttonText: PropTypes.string,
   iconClassName: PropTypes.string,
 };
 
--- stories/index.js
+++ stories/index.js
@@ -2,18 +2,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from '../src/Components/Button/Button';
-import paypalIcon from '../public/Icons/image.svg'
+import paypalIcon from '../public/Icons/image.svg';
+import '../src/index.css';
 
 storiesOf('Button', module)
   .add('Disabled', () => (
-    <Button buttonText="Estimate" className="Disabled-button Test-button-size"/>
+    <Button buttonText="Estimate" buttonTextClassName="Checkout-button-text" className="Checkout-button Disabled-button Test-button-size"/>
   ))
   .add('Light', () => (
     <Button buttonText="Update Bag" className="Light-button Test-button-size"/>
   ))
   .add('Transparent', () => (
-    <Button buttonText="Log in" className="Transparent-button Test-button-size"/>
+    <Button buttonText="Log in" buttonTextClassName="Transparent-button-text" className="Transparent-button Test-button-size"/>
   ))
-  .add('Paypal', () => (
-    <Button buttonIconURL={paypalIcon} iconClassName="Icon-paypal" className="Paypal-button Test-button-size"/>
+  .add('Payment', () => (
+    <Button buttonIconURL={paypalIcon} iconClassName="Icon-paypal" className="Payment-button Test-button-size"/>
+  ))
+  .add('Checkout', () => (
+    <Button buttonText="Checkout now" buttonTextClassName="Checkout-button-text" className="Checkout-button Test-button-size"/>
   ));
